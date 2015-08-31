20619a44c91cf76117772b70805c5f97ac8576fa
kswizzlek <kid7kruger@gmail.com>
2015-08-31 15:09:08 -0400

    new kevinkruger website

---
+++ BETA/kevinK.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function(){
+    $("#mainmenu").click(function(){
+        $("#mainmenu").animate({
+        left: '0%',
+       
+        top: '0%',
+        margin: '0 0 0 0'
+      
+        });
+    	
+ });
+ 
+  $("#one").click(function(){
+  $("#onebody").slideDown();
+});
+});
