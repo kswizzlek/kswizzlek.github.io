6332e18a77b36abb2a01fbde946980099e8df08b
kswizzlek <kid7kruger@gmail.com>
2015-10-01 08:59:03 -0400

    website content edit
    Website content edit

--- BETA/kevinK.js
+++ BETA/kevinK.js
@@ -2,12 +2,15 @@
     $("#mainmenu").click(function(){
         $("#mainmenu").animate({
         left: '0%',
-       
         top: '0%',
         margin: '0 0 0 0'
       
         });
-    	
+    	$(".menutile").animate({
+        width: '100%',
+      
+        });
+
  });
  
   $("#one").click(function(){
